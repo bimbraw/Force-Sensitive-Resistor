e35a73e8d8e37027181636ad09fadedeab660008
Keshav Bimbraw <bimbrawkeshav@gmail.com>
2019-02-27 15:48:10 -0500

    Add files via upload

---
+++ FSR.ino
@@ -0,0 +1,51 @@
+const int FSR_PIN = A0; // Pin connected to FSR/resistor divider
+
+// Measure the voltage at 5V and resistance of your 3.3k resistor, and enter
+// their value's below:
+const float VCC = 4.98; // Measured voltage of Ardunio 5V line
+const float R_DIV = 3230.0; // Measured resistance of 3.3k resistor
+
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(FSR_PIN, INPUT);
+}
+
+void loop() 
+{
+  int fsrADC = analogRead(FSR_PIN);
+  // If the FSR has no pressure, the resistance will be
+  // near infinite. So the voltage should be near 0.
+  //if (fsrADC != 0) // If the analog reading is non-zero
+  //{
+    // Use ADC reading to calculate voltage:
+    float fsrV = fsrADC * VCC / 1023.0;
+    // Use voltage and static resistor value to 
+    // calculate FSR resistance:
+    float fsrR = R_DIV * (VCC / fsrV - 1.0);
+    //Serial.println("Resistance: " + String(fsrR) + " ohms");
+    // Guesstimate force based on slopes in figure 3 of
+    // FSR datasheet:
+    float force;
+    float fsrG = 1.0 / fsrR; // Calculate conductance
+    // Break parabolic curve down into two linear slopes:
+    if (fsrR <= 600) 
+      force = (fsrG - 0.00075) / 0.00000032639;
+    else
+      force =  fsrG / 0.000000642857;
+      force = force * 0.00980665;
+    //Serial.println("Force: " + String(force) + " g");
+    force = force * (6.96/17.70);
+    //Serial.println("Force: " + String(force) + " N");
+    Serial.println(force);
+    //Serial.println();
+
+    delay(10);
+  //}
+  //else
+  //{
+    //  force = 0;
+      //Serial.println("Force: " + String(force) + " N");
+
+  //}
+}
